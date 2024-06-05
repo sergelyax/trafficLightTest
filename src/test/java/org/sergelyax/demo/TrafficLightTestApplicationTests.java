bd98b3c8505b7f05bbeccaec1db26259efd18a8e
sergelyax <1758818@gmail.com>
2024-06-05 17:24:07 +0300

    first commit

---
+++ src/test/java/org/sergelyax/demo/TrafficLightTestApplicationTests.java
@@ -0,0 +1,13 @@
+package org.sergelyax.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrafficLightTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
